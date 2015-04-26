b57a89d1e06675a6c55eb31f1f9699b0897506bd
Chris Orem <chrisorem@gmail.com>
2015-04-26 15:10:30 -0400

    adjust and e stop

--- src/node/QR_Nav.py
+++ src/node/QR_Nav.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import String
+from neato_node.msg import Button
 from geometry_msgs.msg import Twist
 # roslib.load_manifest('my_opencv')
 
@@ -10,10 +11,17 @@ class neato_turn:
     def __init__(self):
         rospy.init_node('QR_Nav', anonymous=True)
         rospy.Subscriber("QR", String, self.callback)
+        rospy.Subscriber("soft_button", Button, self.bcall)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.pub2 = rospy.Publisher('neato_turn', String, queue_size=1)
         self.state = False
 
+    def bcall(self, data):
+
+        if data.value:
+            print 'stop'
+            self.state = False
+            self.callback("test Stop")
 
     def callback(self, data):
         rate = rospy.Rate(.5)  # 1/2 hz
@@ -28,13 +36,13 @@ class neato_turn:
         twist.linear.z = 0
 
         if temp == 'Left':
-            twist.angular.z = .825
+            twist.angular.z = .65
             self.pub2.publish('Turning')
             self.pub.publish(twist)
             rate.sleep()
             self.pub2.publish('Done')
         elif temp == 'Right':
-            twist.angular.z = -0.79
+            twist.angular.z = -0.65
             self.pub2.publish('Turning')
             self.pub.publish(twist)
             rate.sleep()
@@ -48,15 +56,25 @@ class neato_turn:
             self.state = True
         elif temp == 'Adjust':
             print 'Adjusting'
-            if temp[2] == "Left":
-                th = .01
-            if temp[2] == "Right":
-                th = -.01
+            tmp = str(data).split(" ")[2]
+            if tmp == "Left":
+                th = .05
+            if tmp == "Right":
+                th = -.05
 
         if self.state:
             twist.angular.z = .00825+th
             twist.linear.x = .10
             self.pub.publish(twist)
+        else:
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = 0
+            twist.linear.x = 0
+            twist.linear.y = 0
+            twist.linear.z = 0
+            self.pub.publish(twist)
+
 
         return
 
